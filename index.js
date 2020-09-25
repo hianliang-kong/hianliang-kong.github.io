74f5c3814b557073cf60de8aeee043d16b4000d0
hianliang-kong <71876171+hianliang-kong@users.noreply.github.com>
2020-09-25 21:56:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ var connected = true;
 // reorder printers
 // checkbox for minimizing and not updating a printer
 
+function createCookie(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
 window.onload = function(){
   // get saved printers
   reloadPrinters();
