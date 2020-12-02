d3516c2412f7792279690db97f6dc5c76331412e
hianliang-kong <71876171+hianliang-kong@users.noreply.github.com>
2020-12-02 12:24:58 +0800

    Update default.js

--- default.js
+++ default.js
@@ -1,18 +1,23 @@
 var defaultPrinters = {
     "ip": [
-        "http://1cc52d1569e1.ngrok.io/"
+        "http://1cc52d1569e1.ngrok.io/",
+        "1cc52d1569e1.ngrok.io/2"
     ],
     "port": [
+        "80",
         "80"
     ],
     "apikey": [
+        "F9B8FF78EAAD4003B11E9FB1200BCFBF",
         "F9B8FF78EAAD4003B11E9FB1200BCFBF"
     ],
     "noConn": [],
     "cameraIp": [
-        "http://1cc52d1569e1.ngrok.io/"
+        "http://1cc52d1569e1.ngrok.io/",
+        "1cc52d1569e1.ngrok.io/"
     ],
     "camPort": [
+        "8080",
         "8080"
     ]
 }
